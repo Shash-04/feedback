95a42b7f9b5f6508c00be539cdfda4c70fd8689c
Shash-04 <shashwatvaish2@gmail.com>
2025-07-24 01:48:09 +0530

    temp fix for error tls net fs dns error

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   eslint: {
+  eslint: {
     ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.resolve = {
+      ...(config.resolve || {}),
+      fallback: {
+        ...(config.resolve?.fallback || {}),
+        tls: false,
+        net: false,
+        fs: false,
+        crypto: false,
+        dns: false,
+      },
+    };
+    return config;
   },
 };
 
